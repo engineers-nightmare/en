6de909f7cf01839fca60114f379a726ddeffbe3e
Caleb Anderson <canderson@flagshipbio.com>
2017-10-14 14:52:37 -0600

    genned components, remove renderable, remove mesh from door

--- src/component/component_manager.h
+++ src/component/component_manager.h
@@ -4,7 +4,6 @@
 #include <glm/glm.hpp>
 
 #include "c_entity.h"
-#include "../mesh.h"
 
 /* fwd */
 class btRigidBody;
--- src/component/component_system_manager.h
+++ src/component/component_system_manager.h
@@ -1,11 +1,6 @@
 #pragma once
 
 #include "../common.h"
-#include "../chunk.h"
-#include "../mesh.h"
-#include "../render_data.h"
-#include "../ship_space.h"
-#include "../player.h"
 #include "sensor_comparator_component.h"
 #include "gas_production_component.h"
 #include "light_component.h"
@@ -14,7 +9,6 @@
 #include "power_provider_component.h"
 #include "pressure_sensor_component.h"
 #include "relative_position_component.h"
-#include "renderable_component.h"
 #include "surface_attachment_component.h"
 #include "switch_component.h"
 #include "type_component.h"
@@ -30,7 +24,6 @@ extern relative_position_component_manager pos_man;
 extern power_component_manager power_man;
 extern power_provider_component_manager power_provider_man;
 extern pressure_sensor_component_manager pressure_man;
-extern renderable_component_manager render_man;
 extern surface_attachment_component_manager surface_man;
 extern switch_component_manager switch_man;
 extern type_component_manager type_man;
@@ -45,35 +38,35 @@ extern const char *comms_msg_type_sensor_comparison_state;
 extern const char *comms_msg_type_proximity_sensor_state;
 
 
-void
-tick_gas_producers(ship_space *ship);
-
-void
-tick_doors(ship_space *ship);
-
-void
-tick_power_consumers(ship_space *ship);
-
-void
-tick_light_components(ship_space *ship);
-
-void
-tick_pressure_sensors(ship_space *ship);
-
-void
-tick_sensor_comparators(ship_space *ship);
-
-void
-tick_readers(ship_space *ship);
-
-void
-tick_proximity_sensors(ship_space *ship, player *pl);
-
-void
-draw_renderables(frame_data *frame);
-
-void
-draw_doors(frame_data *frame);
-
-void
-set_door_state(ship_space *ship, c_entity ce, surface_type s);
+//void
+//tick_gas_producers(ship_space *ship);
+//
+//void
+//tick_doors(ship_space *ship);
+//
+//void
+//tick_power_consumers(ship_space *ship);
+//
+//void
+//tick_light_components(ship_space *ship);
+//
+//void
+//tick_pressure_sensors(ship_space *ship);
+//
+//void
+//tick_sensor_comparators(ship_space *ship);
+//
+//void
+//tick_readers(ship_space *ship);
+//
+//void
+//tick_proximity_sensors(ship_space *ship, player *pl);
+//
+//void
+//draw_renderables(frame_data *frame);
+//
+//void
+//draw_doors(frame_data *frame);
+//
+//void
+//set_door_state(ship_space *ship, c_entity ce, surface_type s);
